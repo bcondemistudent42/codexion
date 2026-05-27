394bb0e6f26361f93d12b838eec4e4375c3c9cc2
Balthazar Condemine <you@example.com>
2026-05-27 14:08:07 +0200

    struct working to start the thread and every stuff

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bcondemi <bcondemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 17:42:19 by bcondemi          #+#    #+#             */
-/*   Updated: 2026/05/26 20:07:11 by bcondemi         ###   ########.fr       */
+/*   Updated: 2026/05/27 14:07:23 by bcondemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,28 @@
 
 
 void assign_const(char **argv, int utils_const[]);
-t_coder	**assignator_coders(int nb_coder, int utils_const[], t_coder *coders[]);
-void	init_coder(t_coder *coder, int id, int utils_const[], t_coder *coders[]);
+int	assignator_coders(int utils_const[], t_coder *coders[]);
+void	loop_on_coder(int utils_const[], t_coder *coders[]);
+void	init_coder(int index, int utils_const[], t_coder *coders[]);
 
 int	main(int ac, char **argv)
 {
 	int utils_const[7];
-	// to see if needed do this int utils_const[6 * sizeof(t_coder)];
-	
 	t_coder *coders[atoi(argv[1])];
 
 	if (parser_manager(ac, argv) == -1)
 	return (ft_error());
 
-	// to if null return error
 	assign_const(argv, utils_const);
 	// to if null return error
-	assignator_coders(atoi(argv[1]), utils_const, coders);
+	assignator_coders(utils_const, coders);
+	loop_on_coder(utils_const, coders);
 	int j = 0;
-	while (coders[j])
+	while (j < atoi(argv[1]))
 	{
-		printf("index == %d, elt == %d\n", j, coders[j]->id);
+		printf("elt == %d, left == %d, right == %d\n", coders[j]->id, coders[j]->left->id,coders[j]->right->id);
 		j++;
 	}
-	// while (utils_const[j])
-	// {
-		// printf("index == %d,value == %d\n\n", j, utils_const[j]);
-		// j++;
-	// }
-	
 	
 }
 
@@ -57,48 +50,63 @@ void assign_const(char **argv, int utils_const[])
 	utils_const[DONGLE_COOLDOWN] = atoi(argv[7]);
 }
 
-t_coder	**assignator_coders(int nb_coder, int utils_const[], t_coder *coders[])
+int	assignator_coders(int utils_const[], t_coder *coders[])
 {
 	int	i;
 	t_coder	*my_coder;
 
 	// int *coders to assign each coder in a array index
 	i = 0;
-	while (i < nb_coder)
+	while (i < utils_const[NB_CODERS])
 	{
 		my_coder = malloc(sizeof(t_coder));
 		if (my_coder == NULL)
-			return (NULL);
-		init_coder(my_coder, i, utils_const, coders);
+		// to free all element before return
+			return (-1);
 		coders[i] = my_coder;
 		i++;
 	}
-	return (coders);
+	return (0);
 }
 
 
-void	init_coder(t_coder *coder, int id, int utils_const[], t_coder *coders[])
+void	init_coder(int index, int utils_const[], t_coder *coders[])
 {
-	coder->id = id;
-	coder->nb_dongle = 1;
-	coder->compile_cnt = 0;
-	coder->utils_const = utils_const;
-	coder->	last_compile = -1; //to see which value to init
-	if (id == 0)
-		
+	int id;
+
+	id = index + 1;
+	coders[index]->id = id;
+	coders[index]->nb_dongle = 1;
+	coders[index]->compile_cnt = 0;
+	coders[index]->utils_const = utils_const;
+	coders[index]->	last_compile = -1; //to see which value to init
+	if (index == 0)
 	{
-		coder->left = coders[utils_const[NB_CODERS] - 1]; //handle coders if start and end
-		coder->right = coders[id + 1]; //handle coders if start and end
+		coders[index]->left = coders[utils_const[NB_CODERS] - 1]; //handle coders if start and end
+		coders[index]->right = coders[index + 1]; //handle coders if start and end
 	}
-	else if (id + 1== utils_const[NB_CODERS]) // to see with -1 or no
+	else if (index + 1 >= utils_const[NB_CODERS]) // to see with -1 or no
 	{
-		coder->left = coders[id - 1];
-		coder->right = coders[0];
+		coders[index]->left = coders[index - 1];
+		coders[index]->right = coders[0];
 	}
 	else
 	{
-		coder->left = coders[id - 1];
-		coder->right = coders[id + 1];
+		coders[index]->left = coders[index - 1];
+		coders[index]->right = coders[index + 1];
 	}
-	coder->state = INACTIVE;
+	coders[index]->state = INACTIVE;
+}
+
+
+void	loop_on_coder(int utils_const[], t_coder *coders[])
+{
+	int	i;
+
+	i = 0;
+	while (i < utils_const[NB_CODERS])
+	{
+		init_coder(i, utils_const, coders);
+		i++;
+	}
 }
